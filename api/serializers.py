3df7cfa43629512d8d5b2832ac151f685126a46b
pg-goose <pau.galopa.dev@gmail.com>
2024-03-16 10:35:59 +0100

    Implementació creacio de rutas

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,88 @@
-from rest_framework import serializers
-from ppf.common.models.route import Route
+from typing import Never
+from google.maps.routing_v2 import RoutesClient, ComputeRoutesRequest, ComputeRoutesResponse
 
-class RouteSerializer(serializers.ModelSerializer):
+
+from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import Serializer
+from rest_framework.serializers import CharField, FloatField
+
+from ppf.common.models.route import Route
+from google.maps.routing_v2 import Route as GRoute
+
+
+class RouteSerializer(ModelSerializer):
+
     class Meta:
         model = Route
-        fields = '__all__'
+        fields = "__all__"
+
+    def mapsRouteRequest(self) -> ComputeRoutesRequest:
+        """
+        Creates a ComputeRoutesRequest object based on the serialized data.
+
+        Returns:
+            ComputeRoutesRequest: The created ComputeRoutesRequest object.
+        """
+        assert not self.is_valid(), "Can't create a ComputeRoutesRequest from an invalid serializer"
+
+        computeRoutesRequest = ComputeRoutesRequest()
+        computeRoutesRequest.origin = {
+            "location": {
+                "latLng": {
+                    "latitude": self.data.get("originLat"),
+                    "longitude": self.data.get("originLon"),
+                }
+            }
+        }
+        computeRoutesRequest.destination = {
+            "location": {
+                "latLng": {
+                    "latitude": self.data.get("destinationLat"),
+                    "longitude": self.data.get("destinationLon"),
+                }
+            }
+        }
+        return computeRoutesRequest
+
+    @staticmethod
+    def deserializeComputeRoutesResponse(self, response: ComputeRoutesResponse) -> dict:
+        """
+        Serializes a ComputeRoutesResponse object into the serializer.
+
+        Args:
+            response (ComputeRoutesResponse): The ComputeRoutesResponse object to serialize.
+
+        Returns:
+            Never: This method never returns.
+        """
+        assert isinstance(response, ComputeRoutesResponse), "Wrong type for response parameter"
+
+        route: GRoute = response.routes[0]
+        return {
+            "originLat": route.origin.location.latLng.latitude,
+            "originLon": route.origin.location.latLng.longitude,
+            "destinationLat": route.destination.location.latLng.latitude,
+            "destinationLon": route.destination.location.latLng.longitude,
+            "polyline": route.polyline,
+            "duration": route.duration,
+            "departureTime": route.departureTime,
+        }
+
+
+class PreviewRouteSerializer(RouteSerializer):
+    class Meta:
+        model = Route
+        fields = [
+            "originLat",
+            "originLon",
+            "destinationLat",
+            "destinationLon",
+            "polyline",
+            "duration",
+            "departureTime",
+        ]
+        extra_kwargs = {
+            "polyline": {"required": False},
+            "duration": {"required": False},
+            "departureTime": {"required": False},
+        }
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ This module contains the views for the API endpoints related to routes.
 """
 
 import logging
+
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.generics import GenericAPIView
@@ -11,16 +12,27 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.status import HTTP_200_OK
-from api.serializers import RouteSerializer
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.exceptions import ValidationError
+from api.serializers import PreviewRouteSerializer, RouteSerializer
+from api.serializers import CreateRouteSerializer
 from ppf.common.models.route import Route
 
 from .service.route import *
+from .service.googleMaps import computeRoute
+
+from google.maps.routing_v2 import RoutesClient
+
+from ppf.common.models.user import Driver
+
+GoogleMapsRouteClient = RoutesClient()
 
 
 class RouteListCreateView(ListCreateAPIView):
     """
     List and create routes.
-    The route list will only contain a simplified version of all the active routes.
+    When creating a route, if the preview parameter is set to true, the route will not be saved in the
+    database.
     URIs:
     - GET  /routes
     - POST /routes
@@ -28,6 +40,39 @@ class RouteListCreateView(ListCreateAPIView):
 
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+
+    def post(self, request: Request, *args, **kwargs):
+        preview: bool = request.data.get("preview")
+        data: dict = request.data.get("route")
+
+        if preview:
+            routeSerializer = PreviewRouteSerializer(data=data)
+        else:
+            routeSerializer = RouteSerializer(data=data)
+
+        if not routeSerializer.is_valid():
+            raise ValidationError("Invalid request body")
+
+        # Transform the recieved data into a format that the Google Maps API can understand and send the request
+        mapsPayload = routeSerializer.mapsRouteRequest()
+        mapsRoute = computeRoute(mapsPayload, GoogleMapsRouteClient)
+
+        # Deserialize the response from the Google Maps API and send it back to the client
+        computeRoutesResponseData = RouteSerializer.deserializeComputeRoutesResponse(mapsRoute)
+
+        # Once we have the google maps data
+        # If it's a preview, we just return the data, if not, we create the route and return the data
+        if preview:
+            return Response(computeRoutesResponseData, status=HTTP_200_OK)
+
+        # We need both the data from google maps and the data from the original request to create the route
+        complete = {**data, **computeRoutesResponseData}
+        # complete["price"] = computePrice(complete["duration"])
+
+        routeSerializer = RouteSerializer(data=data)
+        routeSerializer.is_valid(raise_exception=True)
+        routeSerializer.create()
+        return Response(routeSerializer.data, status=HTTP_201_CREATED)
 
 
 class RouteRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
@@ -48,7 +93,6 @@ class RouteCancelView(GenericAPIView):
     - Set the route as cancelled.
     - Notify the passenjers that the route was cancelled.
     - Issue a refund to the passenjers.
-    - TODO : TBD more actions
 
     URIs:
     - POST /routes/{id}/cancel
@@ -78,10 +122,6 @@ class RouteJoinView(GenericAPIView):
     """
 
     def post(self, request: Request, *args, **kwargs):
-        logging.debug(f"{request}")
-        logging.debug(f"{args}")
-        logging.debug(f"{kwargs}")
-
         routeId = request.data.get("routeId")
         passenjerId = request.data.get("passenjerId")
 
