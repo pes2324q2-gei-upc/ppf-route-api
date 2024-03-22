53b3705c61e8c83c3a119f07db2a561d599b5e48
pg-goose <pau.galopa.dev@gmail.com>
2024-03-22 03:41:19 +0100

    APIViews para join y leave

--- api/views.py
+++ api/views.py
@@ -5,14 +5,21 @@ This module contains the views for the API endpoints related to routes.
 
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, CreateAPIView
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, CreateAPIView, GenericAPIView
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
-from .service.route import computeMapsRoute
-from common.models.route import Route
+from .service.route import computeMapsRoute, joinRoute, leaveRoute
+from common.models.route import Route, RoutePassenger
 from api.serializers import (
     CreateRouteSerializer,
     PreviewRouteSerializer,
@@ -28,6 +35,9 @@ class RouteRetrieveView(RetrieveAPIView):
     - GET  /routes/{id}
     """
 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Route.objects.all()
     serializer_class = DetaliedRouteSerializer
 
@@ -39,10 +49,12 @@ class RoutePreviewView(CreateAPIView):
     - POST /routes/preview
     """
 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = PreviewRouteSerializer
 
     def post(self, request: Request, *args, **kargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data={**request.data, "driver": request.user.id})
 
         if not serializer.is_valid(raise_exception=True):
             return Response(status=HTTP_400_BAD_REQUEST)
@@ -65,6 +77,9 @@ class RouteListCreateView(ListCreateAPIView):
     - POST /routes
     """
 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         return Route.objects.filter(cancelled=False)
 
@@ -81,7 +96,7 @@ class RouteListCreateView(ListCreateAPIView):
     def post(self, request: Request, *args, **kargs):
         # TODO search for a cached route to not duplicate the route request to maps api
 
-        serializer: CreateRouteSerializer = self.get_serializer(data=request.data)
+        serializer: CreateRouteSerializer = self.get_serializer(data={**request.data, "driver": request.user.id})
         if not serializer.is_valid():
             return Response(status=HTTP_400_BAD_REQUEST)
 
@@ -89,9 +104,59 @@ class RouteListCreateView(ListCreateAPIView):
         mapsResponseData = computeMapsRoute(serializer)
 
         # We need both the data from google maps and the data from the original request to create the route
-        completeSerializer = DetaliedRouteSerializer(data={**serializer.data, **mapsResponseData})
+        completeSerializer = DetaliedRouteSerializer(
+            data={**serializer.data, **mapsResponseData, "driver": request.user.id}
+        )
+        print(completeSerializer)
 
-        if not completeSerializer.is_valid():
+        if not completeSerializer.is_valid(raise_exception=True):
             return Response(status=HTTP_500_INTERNAL_SERVER_ERROR)
         completeSerializer.save()
         return Response(completeSerializer.data, status=HTTP_201_CREATED)
+
+
+class RouteJoinView(CreateAPIView):
+    """
+    Join a route
+    URI:
+    - POST /routes/{id}/join
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        routeId = self.kwargs["pk"]
+        userId = request.user.id
+        joinRoute(routeId, userId)
+        return Response(status=HTTP_200_OK)
+
+
+class RouteLeaveView(CreateAPIView):
+    """
+    Leave a route
+    URI:
+    - POST /routes/{id}/leave
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        routeId = self.kwargs["pk"]
+        userId = request.user.id
+        leaveRoute(routeId, userId)
+        return Response(status=HTTP_200_OK)
+
+
+class RouteCancellView(CreateAPIView):
+    """
+    Join a route
+    URI:
+    - POST /routes/{id}/cancell
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Route.objects.all()
