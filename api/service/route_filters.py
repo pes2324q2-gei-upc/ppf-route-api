8ff981cc02658dc33ac6d67a9a758e362ad4fa6d
pg-goose <pau.galopa.dev@gmail.com>
2024-04-29 00:58:12 +0200

    improved filter descriptions for swagger generation

--- api/service/route_filters.py
+++ api/service/route_filters.py
@@ -6,7 +6,19 @@ from django_filters import CharFilter
 
 
 class BaseRouteFilter(FilterSet):
-    user = CharFilter(method="userFilter")
+    user = CharFilter(
+        method="userFilter",
+        label="A user id that belongs to the route, both as passenger or driver",
+    )
+    driver = CharFilter(field_name="driver_id")
+    passengers = CharFilter(
+        field_name="passengers_id",
+        lookup_expr="in",
+        label="List of user Ids that 'could' be passengers of a route",
+    )
+    seats = CharFilter(
+        field_name="freeSeats", lookup_expr="gte", label="Minimum number of free seats"
+    )
 
     def userFilter(self, queryset, name, value):
         return queryset.filter(Q(driver__id=value) | Q(passengers__id__in=value))
@@ -18,7 +30,4 @@ class BaseRouteFilter(FilterSet):
             "originLon": ["exact"],
             "destinationLat": ["exact"],
             "destinationLon": ["exact"],
-            "driver__id": ["exact"],
-            "passengers__id": ["in"],
-            "freeSeats": ["gte"],
         }
