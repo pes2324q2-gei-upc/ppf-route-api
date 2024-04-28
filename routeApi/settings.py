f7ca7817e41a924c0dc4be9973b1260a742d2f55
pg-goose <pau.galopa.dev@gmail.com>
2024-04-29 00:58:20 +0200

    Added pagination config

--- routeApi/settings.py
+++ routeApi/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 1,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
