c3e0b3ee948680eafc06777930571c8850e09bad
Pecmecy <eldomi99@hotmail.es>
2024-04-29 22:15:53 +0200

    [#169] Add populating script

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
+
+   # def ready(self):
+   #    call_command("seed")    # Seed the database
+   #    return super().ready()
---
+++ api/management/commands/seed.py
@@ -0,0 +1,45 @@
+import requests
+from django.core.management.base import BaseCommand
+from common.models.charger import LocationCharger, ChargerVelocity, ChargerLocationType
+
+
+class Command(BaseCommand):
+    help = 'Populates the database with charger data from the API'
+
+    def handle(self, *args, **options):
+        response = requests.get(
+            'https://analisi.transparenciacatalunya.cat/resource/tb2m-m33b.json')
+        data = response.json()
+
+        for item in data:
+
+            # Create LocationCharger object
+            charger = LocationCharger(
+                promotorGestor=item['promotor_gestor'],
+                access=item['acces'],
+                kw=item['kw'],
+                acDc=item['ac_dc'],
+                latitud=item['latitud'],
+                longitud=item['longitud'],
+                adreA=item['adre_a']
+            )
+            charger.save()
+
+            # Add connection types
+            connection_types = item['tipus_connexi'].split('+')
+            for connection_type in connection_types:
+                charger_type = ChargerLocationType.objects.get(
+                    type=connection_type.strip()
+                )
+                charger.connectionType.add(charger_type)
+
+            # Add velocities
+            velocities = item['tipus_velocitat'].split(' i ')
+            for velocity in velocities:
+                charger_velocity = ChargerVelocity.objects.get(
+                    velocity=velocity
+                )
+                charger.velocities.add(charger_velocity)
+
+    def clear_data(self):
+        LocationCharger.objects.all().delete()
