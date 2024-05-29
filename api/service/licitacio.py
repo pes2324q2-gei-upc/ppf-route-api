40a25bf7500c12c9b0b6e84beef159d6fc3e8b02
Pecmecy <eldomi99@hotmail.es>
2024-05-29 20:33:37 +0200

    [hotfix] Fix serializer for licitacio service

--- api/service/licitacio.py
+++ api/service/licitacio.py
@@ -2,13 +2,13 @@ def serializeLicitacio(charger):
     data = {
         "latitud": charger.latitud,
         "longitud": charger.longitud,
-        "nom_organ": charger.promotorGestor,
+        "nomOrgan": charger.promotorGestor,
         "tipus": "Servei",
         "procediment": "Obert",
-        "fase_publicacio": "Formalització",
-        "denominacio": "Servei de recàrrega de vehicles elèctrics",
-        "lloc_execucio": charger.adreA,
-        "nom_ambit": "PowerPathFinder",
+        "fasePublicacio": "Formalització",
+        "denominacio": "Servei de recàrrega de vehicles elèctrics espatllat",
+        "llocExecucio": charger.adreA,
+        "nomAmbit": "PowerPathFinder",
         "pressupost": 3.69,
     }
     return data
